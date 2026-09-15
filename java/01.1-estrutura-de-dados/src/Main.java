e8c5af56b52562d45e46eb0ebdda42bae3eb4c89
Wallace Santana <wallacesantanak0@gmail.com>
2026-09-15 18:52:54 -0300

    feat(./Task): create example for priorityQueue

--- java/01.1-estrutura-de-dados/src/Main.java
+++ java/01.1-estrutura-de-dados/src/Main.java
@@ -1,15 +1,21 @@
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
     static void main(String[] args) {
-        Queue<String> queue = new LinkedList<>();
+        PriorityQueue<Task> queueTask = new PriorityQueue<Task>();
 
-        queue.offer("Client 1");
-        queue.offer("Client 2");
-        System.out.printf(queue.poll());
+        queueTask.add(new Task("Responder emails", 3));
+        queueTask.add(new Task("Corrigir Bugs", 1));
+        queueTask.add(new Task("Planejamento Sprint", 2));
 
+        System.out.println("Tarefas por ordem de prioridade");
+
+        while (!queueTask.isEmpty()) {
+            System.out.println(queueTask.poll());
+        }
 
     }
 }
---
+++ java/01.1-estrutura-de-dados/src/Task.java
@@ -0,0 +1,20 @@
+public class Task implements Comparable<Task> {
+    String name;
+    int priority;
+
+    public Task(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+
+    @Override
+    public int compareTo(Task t) {
+        return Integer.compare(this.priority, t.priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Prioridade: " + priority + " - Tarefa: " + name;
+    }
+}

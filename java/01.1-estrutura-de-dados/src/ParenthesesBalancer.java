853d22fb7a58bc670f64aa14ec8db7ca259b8618
Wallace Santana <wallacesantanak0@gmail.com>
2026-09-17 15:49:34 -0300

    feat(ParenthesesBalancer): create logic with stack strutucture

---
+++ java/01.1-estrutura-de-dados/src/ParenthesesBalancer.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class ParenthesesBalancer {
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : expression.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+
+            }
+        }
+
+
+         return stack.isEmpty();
+    }
+
+
+    static void main(String[] args) {
+        String[] expressions = {
+                "(a + b)",
+                "((a - b) * c)",
+                "(a * (b + c)"
+        };
+        for (String c : expressions) {
+            System.out.println("A Expressao " + c + " esta balanceada?: " + isBalanced(c));
+
+        }
+    }
+
+}

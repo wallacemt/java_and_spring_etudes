87910639646cffa941ebac07629956e2f21414f9
Wallace Santana <wallacesantanak0@gmail.com>
2026-09-17 15:37:31 -0300

    feat(./LinearSearch): create example for linear search

---
+++ java/01.1-estrutura-de-dados/src/LinearSearch.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+
+    public static int search(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    static void main(String[] args) {
+        int[] nums = {1, 45, 56, 22, 204, 102, 22, 9};
+        System.out.println(search(nums, 204));
+    }
+}
